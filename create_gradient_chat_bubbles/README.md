f61484df576fbed53a4cdfcc385c15abeaa4015f
Niranjan-KS <niranjan.ks.pro@gmail.com>
2023-07-31 12:34:49 +0530

    commit

---
+++ create_gradient_chat_bubbles/README.md
@@ -0,0 +1,3 @@
+# create_gradient_chat_bubbles
+
+A new Flutter project.
---
+++ create_gradient_chat_bubbles/android/app/src/main/kotlin/com/example/create_gradient_chat_bubbles/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.create_gradient_chat_bubbles
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
